906912e1e9dbd7aa5633cdab0b0b25ca62ca8a06
RushilVislawath <rushil.vislawath@gmail.com>
2023-01-22 16:41:51 -0700

    Added description and styled the application

--- ui.py
+++ ui.py
@@ -21,15 +21,18 @@ class ui_window(object):
         self.tabWidget.addTab(self.savedAirportsTab, "Saved")
         self.control_airport_tab()
     def control_airport_tab(self):
-        self.descLabel = QLabel(self.findAirportsTab)
-        self.descLabel.setText("Airport Description:")
-        self.descLabel.move(350, 60)
-        self.descLabel.setStyleSheet("font-size: 20pt")
+        self.titleDescLabel = QLabel(self.findAirportsTab)
+        self.titleDescLabel.setText("Airport Description:")
+        self.titleDescLabel.move(350, 60)
+        self.titleDescLabel.setStyleSheet("font-size: 20pt")
         self.titleLabel = QLabel(self.findAirportsTab)
         self.titleLabel.setText("Salt Lake City International Airport")
         self.titleLabel.move(180, 0)
         self.titleLabel.setStyleSheet("font-weight: bold; font-size: 22pt")
-
+        self.descLabel = QLabel(self.findAirportsTab)
+        self.descLabel.setText(" Salt Lake City International Airport is a civil-military airport \n located about 4 miles west of Downtown Salt Lake City,  \n Utah, in the United States. The airport is the closest\n commercial airport for more than 2.5 million people and is\n within a 30-minute drive of nearly 1.3 million jobs.")
+        self.descLabel.move(260, 100)
+        self.descLabel.setStyleSheet("font-size: 13.5pt")
 
 
 
